b2ab8db068c06f7d47dbfb350733f2e6dc98c6a5
Anirudha <beleanirudha8@gmail.com>
2024-08-02 21:40:04 +0530

    add SASS to check parcel

--- src/index.js
+++ src/index.js
@@ -1,7 +1,20 @@
+import "./styles/index.scss"
+
 console.log("changed");
 console.log("anirudha bele");
 console.log("rohit bele");
 
+
 if (module.hot) {
     module.hot.accept();
 }
+
+
+const obj = {
+    name: "Anirudha",
+    details: {
+        age: 22,
+        status: "unmarried"
+    }
+}
+console.log(obj?.details?.age, obj?.wife?.name);
