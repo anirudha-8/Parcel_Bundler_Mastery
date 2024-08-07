6097fb79731e2c131c7972f630f38e53cebece20
Anirudha <beleanirudha8@gmail.com>
2024-08-07 12:05:38 +0530

    add vue, pug, stylus

---
+++ src/vue/App.vue
@@ -0,0 +1,45 @@
+<template lang="pug">
+    .container
+        h2 Vue!
+        h2 Let's Count! {{ counter }}
+        ul
+            li
+                a(href="/index.html") Home
+</template>
+
+<script setup lang="ts">
+import { ref, onMounted } from "vue";
+
+// Declare a reactive counter variable
+const counter = ref(0);
+
+// Use onMounted to set up the interval
+onMounted(() => {
+	setInterval(() => {
+		counter.value++;
+	}, 1000);
+});
+</script>
+
+<style lang="stylus" scoped>
+bgStyle = darkslategray
+textStyle = aquamarine
+
+.container
+    display: flex
+    align-items: center
+    justify-content: center
+    flex-direction: column
+    width: 900px
+    padding: 40px
+    font-family: sans-serif
+    background-color: bgStyle
+    color: textStyle
+
+    h2
+        font-size: 64px
+
+    a
+        font-size: 28px
+        color: mintcream
+</style>
---
+++ src/vue/index.js
@@ -0,0 +1,11 @@
+// import Vue from 'vue';
+// import App from './App.vue';
+
+// new Vue({
+//     render: h => h(App),
+// }).$mount("#root");
+
+import { createApp } from 'vue';
+import App from './App.vue';
+
+createApp(App).mount('#root');
